f68a06a6633234efc42dd873cde366ecf61bd288
Simon de Haan <simon@turn.io>
2023-02-19 10:12:30 +0100

    formatting

--- lib/expression/v2/callbacks.ex
+++ lib/expression/v2/callbacks.ex
@@ -1,16 +1,5 @@
 defmodule Expression.V2.Callbacks do
-  @built_ins [
-    "*",
-    "+",
-    "-",
-    ">",
-    ">=",
-    "<",
-    "<=",
-    "/",
-    "^",
-    "=="
-  ]
+  @built_ins ["*", "+", "-", ">", ">=", "<", "<=", "/", "^", "=="]
 
   def callback(_context, built_in, args) when built_in in @built_ins,
     do: apply(Kernel, String.to_existing_atom(built_in), args)
