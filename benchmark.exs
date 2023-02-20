f06a0e22ca0ecd7d114d1f61f0a96138ab14fc16
Simon de Haan <simon@turn.io>
2023-02-20 17:22:48 +0100

    document benchmark.exs

--- benchmark.exs
+++ benchmark.exs
@@ -1,11 +1,13 @@
-
-expression = "Hello @contact.name, you were born in @(YEAR(contact.birthday))"
-contact = %{"name" => "mary", "birthday" => ~U[1920-02-02T00:00:00+0000]}
+# Run with `mix run benchmark.exs` in the console
 
 Benchee.run(
   %{
-    "v1" => fn -> Expression.evaluate(expression, %{"contact" => contact}) end,
-    # "v1.parsing" => fn -> Expression.parse!(expression) end,
-    "v2" =>  fn -> Expression.V2.eval(expression, %{contact: contact}, Expression.V2.Callbacks) end,
-    # "v2.parsing" => fn -> Expression.V2.parse(expression) end
+    "v1" => fn {expression, context} -> Expression.evaluate(expression, context) end,
+    "v2" =>  fn {expression, context} -> Expression.V2.eval(expression, context, Expression.V2.Callbacks) end,
+  }, 
+  inputs: %{
+    "simple" => {
+      "Hello @contact.name, you were born in @(YEAR(contact.birthday))", 
+      %{contact: %{"name" => "mary", "birthday" => ~U[1920-02-02T00:00:00+0000]}}
+    }
   })
