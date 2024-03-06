78a46cef992229b46aae18086ee7f62b8a200bfe
Simon de Haan <simon@turn.io>
2024-03-06 10:57:19 +0100

    fix credo

--- lib/expression.ex
+++ lib/expression.ex
@@ -1,40 +1,8 @@
 defmodule Expression do
   @moduledoc """
-  Documentation for `Expression`, a library to parse and evaluate
-  [Floip](https://floip.gitbook.io/flow-specification/expressions) compatible expressions
+  The default implementation of the Expression library.
 
-  Expression is an expression language which consists of the functions provided
-  by Excel with a few additions.
-
-  Function and variable names are not case-sensitive so UPPER is equivalent to upper:
-
-  ```
-  contact.name -> Marshawn Lynch
-  FIRST_WORD(contact.name) -> Marshawn
-  first_word(CONTACT.NAME) -> Marshawn
-  ```
-
-  For templating, RapidPro uses the @ character to denote either a single variable substitution
-  or the beginning of an Expression block. `@` was chosen as it is known how to type by a broad
-  number of users regardless of keyboard. It does have the disadvantage of being used in
-  email addresses and Twitter handles, but these are rarely ambiguous and escaping can be
-  done easily via doubling of the character (`@@`).
-
-  Functions are called by using the block syntax:
-  ```
-  10 plus 4 is @(SUM(10, 4))
-  ```
-
-  Within a block, `@` is not required to refer to variable in the context:
-  ```
-  Hello @(contact.name)
-  ```
-
-  A template can contain more than one substitution or block:
-  ```
-  Hello @contact.name, you were born in @(YEAR(contact.birthday))
-  ```
-
+  This delegates the required functions to `Expression.V1`.
   """
 
   @behaviour Expression.Behaviour
