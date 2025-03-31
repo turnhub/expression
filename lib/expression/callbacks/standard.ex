daccb77b557d56b4f5dee6a6c2ca3404a85f51ae
João Thallis <joaothallis@icloud.com>
2025-03-30 21:16:16 -0300

    refactor: fix dialyzer issues

--- lib/expression/callbacks/standard.ex
+++ lib/expression/callbacks/standard.ex
@@ -1856,14 +1856,8 @@ defmodule Expression.Callbacks.Standard do
 
   defp extract_numberish(expression) do
     with [match] <-
-           Regex.run(~r/([0-9]+\.?[0-9]*)/u, replace_arabic_numerals(expression), capture: :first),
-         float <- parse_float(match) do
-      float
-    else
-      # Regex can return nil
-      nil -> nil
-      # Float parsing can return :error
-      :error -> nil
+           Regex.run(~r/([0-9]+\.?[0-9]*)/u, replace_arabic_numerals(expression), capture: :first) do
+      parse_float(match)
     end
   end
 
@@ -1979,7 +1973,6 @@ defmodule Expression.Callbacks.Standard do
       float == number
     else
       nil -> false
-      :error -> false
     end
   end
 
@@ -2003,7 +1996,6 @@ defmodule Expression.Callbacks.Standard do
       number > float
     else
       nil -> false
-      :error -> false
     end
   end
 
@@ -2027,7 +2019,6 @@ defmodule Expression.Callbacks.Standard do
       number >= float
     else
       nil -> false
-      :error -> false
     end
   end
 
@@ -2051,7 +2042,6 @@ defmodule Expression.Callbacks.Standard do
       number < float
     else
       nil -> false
-      :error -> false
     end
   end
 
@@ -2077,7 +2067,6 @@ defmodule Expression.Callbacks.Standard do
       number <= float
     else
       nil -> false
-      :error -> false
     end
   end
 
