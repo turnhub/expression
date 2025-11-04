69119d84ceeb984ffc399db177a55d0922b58a3c
nathanbegbie <nathanbegbie@gmail.com>
2025-11-04 15:45:23 +1030

    remove experimental v2 from expressions
    See https://turnio.slack.com/archives/C02EC0PN15G/p1762153270892709

--- mix.exs
+++ mix.exs
@@ -42,7 +42,6 @@ defmodule Expression.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:benchee, "~> 1.0", only: :dev},
       {:credo, "~> 1.5", only: [:dev], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, ">= 0.28.2", only: :dev, runtime: false},
